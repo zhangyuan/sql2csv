63fc74d428efc5123466cbf5bd4c9821b9a1d57c
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-04-28 16:07:30 +0800

    remove the debug info from the executable to make it smaller

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ BINARY_NAME=sql2csv
 
 rm -rf bin/${BINARY_NAME}*
 
-GOOS=windows GOARCH=amd64 go build -o bin/${BINARY_NAME}-amd64.exe
+GOOS=windows GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64.exe
 
-GOOS=windows GOARCH=386 go build -o bin/${BINARY_NAME}-386.exe
+GOOS=windows GOARCH=386 go build -ldflags "-w" -o bin/${BINARY_NAME}-386.exe
 
-GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY_NAME}-amd64-darwin
+GOOS=darwin GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64-darwin
 
-GOOS=linux GOARCH=amd64 go build -o bin/${BINARY_NAME}-amd64-linux
+GOOS=linux GOARCH=amd64 go build -ldflags "-w" -o bin/${BINARY_NAME}-amd64-linux
 
-GOOS=linux GOARCH=386 go build -o bin/${BINARY_NAME}-386-linux
+GOOS=linux GOARCH=386 go build -ldflags "-w" -o bin/${BINARY_NAME}-386-linux
