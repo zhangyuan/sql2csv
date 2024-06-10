c8d0b65977c37d2e17f89710702c31af10aa671f
Yuan Zhang <zhangyuan@users.noreply.github.com>
2024-06-10 23:57:26 +0800

    show more debug info

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,16 @@
 
 set -euo pipefail
 
-export DATABASE_URI=${DATABASE_URI:-postgresql://127.0.0.1/postgres?user=postgres&password=mypassword&sslmode=disable}
+export DATABASE_URI=${DATABASE_URI:-"postgresql://127.0.0.1/postgres?user=postgres&password=mypassword&sslmode=disable"}
 
 mkdir -p tmp/
 
 go run main.go -q "select * from users where 1=1" > tmp/users.csv
+
+echo fixtures/users.csv
+cat fixtures/users.csv
+
+echo tmp/users.csv
+cat tmp/users.csv
 
 diff tmp/users.csv fixtures/users.csv
